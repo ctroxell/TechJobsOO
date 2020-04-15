cbeadb21c96bb7efc3dbaa841d458621f93dd21c
Emma Troxell <troxellemma@gmail.com>
2020-04-15 14:55:53 -0500

    implemented find by id in job controller & index view

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -18,9 +19,8 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
